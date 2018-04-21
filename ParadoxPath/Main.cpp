49324bb88e15e2089f8e77ab096d06273de00594
Nikolai Prigodich <1lampard@mail.ru>
2018-04-21 15:27:20 +0300

    finish paradoxPath task

--- ParadoxPath/Main.cpp
+++ ParadoxPath/Main.cpp
@@ -1,37 +1,25 @@
 #include <iostream>
-#include <tuple>
 #include <vector>
 #include <queue>
-#include <functional>
+#include <set>
+#include <climits>
 
-int idx(int nMapWidth, int x, int y)
+struct NodePathLenght
 {
-	return x + y * nMapWidth;
+	int node;
+	int pathLenght;
+};
+
+bool operator <(const NodePathLenght& left, const NodePathLenght& right)
+{
+	return left.pathLenght > right.pathLenght;
 }
 
-// lower bound distance to target from u
-int costToTarget(int nMapWidth, int nTargetX, int nTargetY, int u)
+int distanceToTarget(int nMapWidth, int nTargetX, int nTargetY, int node)
 {
-	int x = u % nMapWidth, y = u / nMapWidth;
+	const int x = node % nMapWidth;
+	const int y = node / nMapWidth;
 	return abs(x - nTargetX) + abs(y - nTargetY);
-}
-
-struct Node
-{
-	int x;
-	int y;
-};
-
-struct NodePathfindingData
-{
-	int cost;
-	Node parentNode;
-	Node node;
-};
-
-bool operator <(const NodePathfindingData& left, const NodePathfindingData& right)
-{
-	return left.cost > right.cost;
 }
 
 int FindPath(const int nStartX, const int nStartY,
@@ -39,54 +27,75 @@ int FindPath(const int nStartX, const int nStartY,
 	const unsigned char* pMap, const int nMapWidth, const int nMapHeight,
 	int* pOutBuffer, const int nOutBufferSize)
 {
-	const int n = nMapWidth * nMapHeight;
-	const Node startPos = {nStartX, nStartY};
-	const Node targetPos = {nTargetX, nTargetY};
+	const int startNode = nStartX + nMapWidth * nStartY;
+	const int targetNode = nTargetX + nMapWidth * nTargetY;
 
-	int discovered = 0; 
-	int exploredNodesCount = 0;
-	std::vector<int> parents(n);
-	std::vector<int> costs(n, INT_MAX);
-	std::priority_queue<NodePathfindingData> openedNodes; // A* with tie breaking
-	costs[startPos] = 0;
-	openedNodes.push({0 + costToTarget(nMapWidth, nTargetX, nTargetY, startPos), 0, startPos});
+	const int nodesCount = nMapWidth * nMapHeight;
+	std::vector<int> parents(nodesCount);
+	std::vector<int> distances(nodesCount, INT_MAX);
+	
+	std::priority_queue<NodePathLenght> openedNodes;
+	std::set<int> closedNodes;
+
+	distances[startNode] = 0;
+	openedNodes.push( {startNode, distanceToTarget(nMapWidth, nTargetX, nTargetY, startNode)} );
 	while (!openedNodes.empty()) 
 	{
 		const int currentNode = openedNodes.top().node; 
 		openedNodes.pop(); 
-		exploredNodesCount++;
 
-		if (currentNode == targetPos)
+		closedNodes.emplace(currentNode);
+		
+		if (currentNode == targetNode)
 		{
-			if (costs[targetPos] <= nOutBufferSize) 
+			const int distanceToTarget = distances[targetNode];
+
+			if (distanceToTarget <= nOutBufferSize)
 			{
-				NodePathfindingData curr = targetPos;
-				for (int i = costs[targetPos] - 1; i >= 0; i--) 
+				int pathNode = targetNode;
+				for (int i = distanceToTarget - 1; i >= 0; i--) 
 				{
-					pOutBuffer[i] = curr;
-					curr = curr;
+					pOutBuffer[i] = pathNode;
+					pathNode = parents[pathNode];
 				}
-				return costs[targetPos];
 			}
 
-			return costs[targetPos]; // buffer size too small
+			return distanceToTarget;
 		}
 
-		for (auto directions : { +1, -1, +nMapWidth, -nMapWidth }) 
+		const int right = 1;
+		const int left = -1;
+		const int up = -nMapWidth;
+		const int down = nMapWidth;
+		for (int direction : { right, left, down, up }) 
 		{
-			int nearbyNode = currentNode + directions;
-			if ((directions == 1 && (nearbyNode % nMapWidth == 0)) || (directions == -1 && (currentNode % nMapWidth == 0)))
+			const int neighbourNode = currentNode + direction;
+
+			if (neighbourNode < 0 || neighbourNode >= nodesCount)
 			{
 				continue;
 			}
 
-			const bool isTraversable = pMap[nearbyNode] == 1;
-			if (nearbyNode >= 0 && nearbyNode < n && costs[nearbyNode] > costs[currentNode] + 1 && isTraversable) 
+			if ((direction == right && neighbourNode % nMapWidth == 0) || 
+				(direction == left && currentNode % nMapWidth == 0))
 			{
-				parents[nearbyNode] = currentNode;
-				costs[nearbyNode] = costs[currentNode] + 1;				
+				continue;
+			}
+			
+			if (closedNodes.find(neighbourNode) != closedNodes.cend())
+			{
+				continue;
+			}
 
-				openedNodes.push({costs[nearbyNode] + costToTarget(nMapWidth, nTargetX, nTargetY, nearbyNode), ++discovered, nearbyNode});
+			const bool isTraversable = pMap[neighbourNode] == 1;
+			const int newDistanceToNode = distances[currentNode] + 1;
+			if (isTraversable && distances[neighbourNode] > newDistanceToNode) 
+			{
+				parents[neighbourNode] = currentNode;
+				distances[neighbourNode] = newDistanceToNode;
+
+				const int pathLenght = newDistanceToNode + distanceToTarget(nMapWidth, nTargetX, nTargetY, neighbourNode);
+				openedNodes.push( {neighbourNode, pathLenght} );
 			}
 		}
 	}
@@ -96,22 +105,50 @@ int FindPath(const int nStartX, const int nStartY,
 
 int main()
 {
-	unsigned char pMap[] = { 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1 };
-	int pOutBuffer[12];
-	int res = FindPath(0, 0, 1, 2, pMap, 4, 3, pOutBuffer, 12);
-	//FindPath must return 3, and the first three positions of pOutBuffer must be populated with {1, 5, 9}
+	unsigned char pMap1[] = { 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1 };
+	int pOutBuffer1[12];
+	const int res1 = FindPath(0, 0, 1, 2, pMap1, 4, 3, pOutBuffer1, 12);
+	//FindPath must return 3, {1, 5, 9}
 
-//	unsigned char pMap[] = {0, 0, 1, 0, 1, 1, 1, 0, 1};
-//	int pOutBuffer[7];
-//	FindPath(2, 0, 0, 2, pMap, 3, 3, pOutBuffer, 7);
-	//For this input FindPath must return ?1
-	
-	std::cout << res << std::endl;
-
-	for (int i = 0; i < res; ++i)
+	std::cout << res1 << ": ";
+	if (res1 > 0)
 	{
-		std::cout << pOutBuffer[i] << " ";
+		for (int i = 0; i < res1; ++i)
+		{
+			std::cout << pOutBuffer1[i] << " ";
+		}
 	}
+	std::cout << "\n";
+
+	unsigned char pMap2[] = {0, 0, 1, 0, 1, 1, 1, 0, 1};
+	int pOutBuffer2[7];
+	const int res2 = FindPath(2, 0, 0, 2, pMap2, 3, 3, pOutBuffer2, 7);
+	//For this input FindPath must return -1
+	
+	std::cout << res2 << ": ";
+	if (res2 > 0)
+	{
+		for (int i = 0; i < res2; ++i)
+		{
+			std::cout << pOutBuffer2[i] << " ";
+		}
+	}
+	std::cout << "\n";
+
+	unsigned char pMap3[] = { 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1 };
+	int pOutBuffer3[12];
+	const int res3 = FindPath(0, 0, 1, 2, pMap3, 4, 3, pOutBuffer3, 2);
+	//FindPath must return 3, {1, 5, 9}
+
+	std::cout << res3 << ": ";
+	if (res3 > 0)
+	{
+		for (int i = 0; i < res3; ++i)
+		{
+			std::cout << pOutBuffer3[i] << " ";
+		}
+	}
+	std::cout << "\n";
 
 	getchar();
 
